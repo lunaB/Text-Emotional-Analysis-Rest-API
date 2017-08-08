e4d324c3150737ebe99ae755cf232405a7c937d6
lunaCry <luna20617@gmail.com>
2017-08-09 03:59:04 +0900

    crawler

--- src/crawler/NaverMovieCrawler.java
+++ src/crawler/NaverMovieCrawler.java
@@ -9,6 +9,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import common.model.WordDAO;
 import crawler.util.NLP;
 import crawler.util.TwitterNLP;
 
@@ -22,12 +23,12 @@ public class NaverMovieCrawler {
 	
 	public void process(){
 		try {
-			Document doc = Jsoup.connect("http://movie.naver.com/movie/point/af/list.nhn?&page=1").get();
+			// naver 최대  1000페이지
+			Document doc = Jsoup.connect("http://movie.naver.com/movie/point/af/list.nhn?&page="+1).get();
 			Elements contents = doc.select("table.list_netizen>tbody>tr");
 			for(Element ele : contents){
 				int num = Integer.parseInt(ele.select("td.ac").text());
 				int star = Integer.parseInt(ele.select("td.point").text());
-//				String title = ele.select("td.title>a.movie").text();
 				String comment = ele.select("td.title").text();
 				comment = comment.substring(0,comment.length()-3); 	//뒤에 "(space)신고" 제거
 				
@@ -38,9 +39,14 @@ public class NaverMovieCrawler {
 					String word = it.next();
 				}
 			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	private int lastAc(){
+		WordDAO wordDAO = WordDAO.getInstance();
+		wordDao
+	}
 }
